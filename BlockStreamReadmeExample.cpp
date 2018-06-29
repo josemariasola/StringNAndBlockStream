6da0b1d4587281790546ba8c88a16019e5860230
José María Sola <@josemariasola>
2018-06-29 10:11:57 -0300

    Breace initialization, constexpr, Byte type. Fix 30 to 130.

--- BlockStreamReadmeExample.cpp
+++ BlockStreamReadmeExample.cpp
@@ -1,22 +1,24 @@
 #include "BlockStream.h"
 #include <cassert>
-int main(){
-	struct Color{unsigned char r,g,b;};
 
-	auto filename{"color.bin"};
+int main(){
+	using Byte = unsigned char; // std::byte C++17 #include<cstddef>
+	struct Color{Byte red, green, blue;};
+
+	constexpr auto filename{"color.bin"};
 	
-	std::ofstream out(filename, std::ios::binary);
-	WriteBlock(out, Color{70,30,180});
+	std::ofstream out{filename, std::ios::binary};
+	WriteBlock(out, Color{70,130,180});
 	out.close();
 
-	std::ifstream in(filename, std::ios::binary);
+	std::ifstream in{filename, std::ios::binary};
 	Color steelBlue;
 	ReadBlock(in, steelBlue);
 	in.close();
+
+	remove(filename); // C++17 // std::filesystem::remove(filename); 
 	
-	assert( 70 == steelBlue.r);
-	assert( 30 == steelBlue.g);
-	assert(180 == steelBlue.b);
-	
-	//remove(filename); // c++17 // std::filesystem::remove(filename); // Remover el archivo de prueba
+	assert(  70 == steelBlue.red   );
+	assert( 130 == steelBlue.green );
+	assert( 180 == steelBlue.blue  );
 }
