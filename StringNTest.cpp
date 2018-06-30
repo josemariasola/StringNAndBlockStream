e6a4f41cb225718bd4c5ef913d12665a903de4aa
José María Sola <@josemariasola>
2018-06-30 18:35:40 -0300

    Renamed.

--- StringNTest.cpp
+++ StringNTest.cpp
@@ -1,5 +1,12 @@
+/* BlockStreamTest.cpp
+2015-11-09 - 2018-06-30
+Esp. Ing. José María Sola
+Profesor
+UTN FRBA */
+
 #include "StringN.h"
 #include <cassert>
+
 int main(){
 	std::string s{"Hello, World!"}; // Standard unbounded string s.
 	String<5> a = PackString(s);    // Put s into bounded string a.
