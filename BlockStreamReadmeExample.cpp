78387d2eff632e929c7502f29686c869394b162a
José María Sola <@josemariasola>
2018-06-30 15:39:00 -0300

    Adds comments.

--- BlockStreamReadmeExample.cpp
+++ BlockStreamReadmeExample.cpp
@@ -3,18 +3,18 @@
 
 int main(){
 	using Byte = unsigned char; // std::byte C++17 #include<cstddef>
-	struct Color{Byte red, green, blue;};
+	struct Color{Byte red, green, blue;};          // A sample block.
 
 	constexpr auto filename{"color.bin"};
 	
-	std::ofstream out{filename, std::ios::binary};
-	WriteBlock(out, Color{70,130,180});
-	out.close();
+	std::ofstream out{filename, std::ios::binary}; // Connect to write.
+	WriteBlock(out, Color{70,130,180});            // Write to out.
+	out.close();                                   // Disconnect.
 
-	std::ifstream in{filename, std::ios::binary};
-	Color steelBlue;
-	ReadBlock(in, steelBlue);
-	in.close();
+	std::ifstream in{filename, std::ios::binary};  // Connect to read.
+	Color steelBlue;                               // Object to store read data.                   
+	ReadBlock(in, steelBlue);                      // Read from in.
+	in.close();                                    // Disconcect,
 
 	remove(filename); // C++17 // std::filesystem::remove(filename); 
 	
--- StringNReadmeExample.cpp
+++ StringNReadmeExample.cpp
@@ -1,8 +1,8 @@
 #include "StringN.h"
 #include <cassert>
 int main(){
-	std::string s{"Hello, World!"};
-	String<5> a = PackString(s);
-	auto t{UnpackString(a)};
-	assert("Hello" == t);
+	std::string s{"Hello, World!"}; // Standard unbounded string s.
+	String<5> a = PackString(s);    // Put s into bounded string a.
+	auto t{UnpackString(a)};        // Get the string from a.
+	assert("Hello" == t);           // Check.
 }
